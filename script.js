343d474595d54ec806ceccb8951a8cff794f2907
assiajsaad <assia.j.saad@gmail.com>
2024-02-22 22:24:11 +0200

    .

--- script.js
+++ script.js
@@ -14,53 +14,44 @@ function divide(a, b) {
   return a / b;
 }
 
-function switchSign(){
-  let
+function switchSign() {
+  document.querySelector("#display").value *= -1;
 }
-
 
 function appendToDisplay(value) {
-  (document.querySelector("#display").value += value);
+  document.querySelector("#display").value += value;
 }
-
 
 function clear() {
-  (document.querySelector("#display").value = "");
+  document.querySelector("#display").value = "";
 }
 
+let buttons = document.querySelectorAll("button");
 
-
-
-
-// let btnClear = document.querySelector(".clear");
-
-// btnClear.addEventListener("click", clear);
-
-// let numbersBtn = document.querySelectorAll(".number");
-
-// numbersBtn.forEach((number) => {
-//   number.addEventListener("click", () => {
-//     let disp = (document.querySelector("#display").value += number.value);
-//   });
-// });
-
-let buttons = document.querySelectorAll('button');
-
-buttons.forEach((button =>{
+buttons.forEach((button) => {
   switch (button.className) {
-    case 'number':
-      button.addEventListener('click',()=>{
+    case "number":
+      button.addEventListener("click", () => {
         appendToDisplay(button.value);
-      })
+      });
       break;
-    case 'clear':
-      button.addEventListener('click',()=>{
+    case "clear":
+      button.addEventListener("click", () => {
         clear();
-      })
+      });
       break;
-  
+    case "sign":
+      button.addEventListener("click", () => {
+        switchSign();
+      });
+      break;
+    case "operator":
+      button.addEventListener("click", () => {
+        appendToDisplay(button.value);
+      });
+      break;
+
     default:
       break;
   }
-}))
-
+});
