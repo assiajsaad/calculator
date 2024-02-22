69783030478053e23db54da5c2ad429d417ad27f
assiajsaad <assia.j.saad@gmail.com>
2024-02-22 17:36:21 +0200

    Added operation functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(`Add ${add(1,2)} adding 1 + 2`);
+console.log(`Subtract ${subtract(8,10)} subtracting 8 - 10`);
+console.log(`Multiply ${multiply(3,5)} multiply 3 * 5`);
+console.log(`Divide ${divide(-12,4)} divide 12 / 4`);
